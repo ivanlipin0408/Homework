f6b0feebdd42fcc05784b80dac3bc5197b822bf1
Ivan Lipin <robotlink@yandex.ru>
2024-10-17 22:24:38 +0300

    установлены линтеры

--- Scr/processing.py
+++ Scr/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dict: list, state="EXECUTED") -> list:
+def filter_by_state(list_of_dict: list[dict], state="EXECUTED") -> list[dict]:
     """Возвращение новых списков словарей по статусу "Выполнено" или "Отменено" """
 
     filtered_list_of_dict = []
@@ -12,4 +12,12 @@ def filter_by_state(list_of_dict: list, state="EXECUTED") -> list:
                 filtered_list_of_dict.append(dict)
     print(filtered_list_of_dict)
     return filtered_list_of_dict
+
+
+def sort_by_date(list_of_dict: list[dict], sorting_method="Reverse=True"):
+    sorted_list_of_dict = []
+
+
+    print(sorted_list_of_dict)
+    return sorted_list_of_dict
 
